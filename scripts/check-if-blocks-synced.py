806111e7a9d79022ff39c09221d0c7dd89ef13ec
karan <karan@blockwala.io>
2018-06-24 17:58:08 +0530

    indentation

--- scripts/check-if-blocks-synced.py
+++ scripts/check-if-blocks-synced.py
@@ -1,15 +1,15 @@
-    import httplib2
-    from bitcoinrpc.authproxy import AuthServiceProxy
-    access = AuthServiceProxy("http://%s:%s@127.0.0.1:18332"%('karan', 'blockwala@123'))
-    try:
-            blockCount = access.getblockcount()
-    except Exception as e:
-            print "Problems connecting to bitcoin wallet:"
-    else:
-            try:
-                    response, trueBlockCount = httplib2.Http().request("http://blockexplorer.com/q/getblockcount/")
-            except Exception as e:
-                    print "Unable to get true blockcount from blockexplorer:"+str(e)
-            else:
-                    if (int(trueBlockCount) - 5) > blockCount :
-                            print "blockchain not up to date: true block count is: "+str(trueBlockCount)+", while bitcoind is at: "+str(blockCount)
+import httplib2
+from bitcoinrpc.authproxy import AuthServiceProxy
+access = AuthServiceProxy("http://%s:%s@127.0.0.1:18332"%('karan', 'blockwala@123'))
+try:
+        blockCount = access.getblockcount()
+except Exception as e:
+        print "Problems connecting to bitcoin wallet:"
+else:
+        try:
+                response, trueBlockCount = httplib2.Http().request("http://blockexplorer.com/q/getblockcount/")
+        except Exception as e:
+                print "Unable to get true blockcount from blockexplorer:"+str(e)
+        else:
+                if (int(trueBlockCount) - 5) > blockCount :
+                        print "blockchain not up to date: true block count is: "+str(trueBlockCount)+", while bitcoind is at: "+str(blockCount)
