bcfa4790f75a5ab72ad03d05b7ccfbff3e2ad2b9
Ubuntu <ubuntu@ip-172-31-27-95.ap-southeast-1.compute.internal>
2018-06-24 11:06:50 +0000

    prod setup

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ pip install -r requirements.txt
 python3 manage.py migrate --run-syncdb
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver
+python3 manage.py runserver 0.0.0.0:8000
