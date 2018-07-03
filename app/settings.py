6fc73116c5bbe17f42433a854feab8459c5df9c5
Ubuntu <ubuntu@ip-172-31-27-95.ap-southeast-1.compute.internal>
2018-07-03 09:35:50 +0000

    few prod changes

--- app/settings.py
+++ app/settings.py
@@ -149,8 +149,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-if DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 STATIC_URL = '/static/'
--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,5 @@
 #pip install -r requirements.txt
+python3 manage.py migrate --run-syncdb
 python3 manage.py makemigrations
 python3 manage.py migrate
 python manage.py collectstatic
