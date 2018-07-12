f87a4d9af817f3f80cb6a646d7d11273e4c822c5
karan <karan@blockwala.io>
2018-07-12 15:52:33 +0530

    multi thread

--- scripts/migrate_data_output_limits.py
+++ scripts/migrate_data_output_limits.py
@@ -4,10 +4,12 @@ import gc
 import csv
 import json
 import threading
+import time
 from bitcoin_data_app.models import Transaction_Table, Input_Table, Output_Table, Block_Table
 from django.http import JsonResponse
 from django.db import connection
 from app.settings import BLOCK_DATA_DIR
+import math
 #from .. import settings
 ############### Location of directories ####################
 #pass the path for the bitcoin-node data
@@ -19,48 +21,73 @@ from app.settings import BLOCK_DATA_DIR
  than the first time you saved the cache file as the new blocks will not be included in the cache.
 '''
 def get_blocks(start, stop):
-    blockchain = Blockchain(BLOCK_DATA_DIR)
-    print("BLOCKS accessed")
-    print("start "+str(start))
-    print("stop "+str(stop))
+    start = int(start)
+    stop = int(stop)
+    net = stop - start
+    bucket = 20000
+    limit = math.ceil(net / bucket)
 
-    for block in blockchain.get_ordered_blocks(BLOCK_DATA_DIR + '/index', int(start), int(stop), 'index-cache.pickle'):
-        get_tx_table(block)
-        print("For block "+ str(block.height))
+    for i in range(limit):
+        local_start = start + (i*bucket)
+        local_stop = local_start + bucket
+        local_start = str(local_start)
+        local_stop = str(local_stop)
+        thread1 = Mythread(local_start, local_stop)
+        thread1.start()
 
 
-def get_tx_table(block):
-    for tx in block.transactions:
-        get_output_table(tx)
+class Mythread(threading.Thread):
+
+    def __init__(self, start_local, stop_local):
+        threading.Thread.__init__(self)
+        self.start_local = start_local
+        self.stop_local = stop_local
+
+    def run(self):
+        blockchain = Blockchain(BLOCK_DATA_DIR)
+        print("BLOCKS accessed")
+        print("start "+self.start_local)
+        print("stop "+self.stop_local)
+
+        for block in blockchain.get_ordered_blocks(BLOCK_DATA_DIR + '/index', int(self.start_local), int(self.stop_local), 'index-cache.pickle'):
+            self.get_tx_table(block)
+            print("For block "+ str(block.height))
+            time.sleep(0.1)
 
 
-def get_output_table(tx):
-    outputs = []
-    for number, output in enumerate(tx.outputs):
-        try:
-            output_type = output.type
-            output_value = output.value
-            output_size = output.size
-            script_value = output.script.value
-            script_operations = output.script.operations
-            for _address in output.addresses:
-                record = {
-                            'transaction_hash_id': tx.hash,
-                            'output_no':number,
-                            'output_type':output_type,
-                            'output_value':output_value,
-                            'size':output_size,
-                            'address':_address.address,
-                            'output_script_value': script_value,
-                            'output_script_operations': script_operations
-                        }
-                outputs.append(record)
-        except:
-            continue
+    def get_tx_table(self, block):
+        for tx in block.transactions:
+            time.sleep(.001)
+            self.get_output_table(tx)
 
-    Output_Table.objects.bulk_create([
-            Output_Table(**record) for record in outputs
-        ])
+
+    def get_output_table(self, tx):
+        outputs = []
+        for number, output in enumerate(tx.outputs):
+            try:
+                output_type = output.type
+                output_value = output.value
+                output_size = output.size
+                script_value = output.script.value
+                script_operations = output.script.operations
+                for _address in output.addresses:
+                    record = {
+                                'transaction_hash_id': tx.hash,
+                                'output_no':number,
+                                'output_type':output_type,
+                                'output_value':output_value,
+                                'size':output_size,
+                                'address':_address.address,
+                                'output_script_value': script_value,
+                                'output_script_operations': script_operations
+                            }
+                    outputs.append(record)
+            except:
+                continue
+
+        Output_Table.objects.bulk_create([
+                Output_Table(**record) for record in outputs
+            ])
 
     
 def run(*args):
