32af69e2df9c4ca48e2796fcf8e4379677c60f3c
karan <karan@blockwala.io>
2018-08-09 15:43:21 +0530

    block sync script modified

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,1 +1,2 @@
-python manage.py runscript -v2 migrate_data --script-args $1 $2
+#python manage.py runscript -v2 migrate_data --script-args $1 $2
+python manage.py runscript -v2 sync_last_mined_blocks --script-args true
--- scripts/sync_last_mined_blocks.py
+++ scripts/sync_last_mined_blocks.py
@@ -2,32 +2,29 @@ from bitcoinrpc.authproxy import AuthServiceProxy
 import psycopg2
 import subprocess
 
+def sync(debug):
 
+	if debug:
+		sync_loc = "/Users/karanahuja/Workspace/INTERNAL-projects/bitcoin-block-explorer/scripts/sync.sh"
+	else:
+		sync_loc = "/Users/karanahuja/Workspace/INTERNAL-projects/bitcoin-block-explorer/scripts/sync.sh"
 
-DEBUG = True
-
-if DEBUG:
-	sync_loc = "/Users/karanahuja/Workspace/INTERNAL-projects/bitcoin-block-explorer/scripts/sync.sh"
-else:
-	sync_loc = "/Users/karanahuja/Workspace/INTERNAL-projects/bitcoin-block-explorer/scripts/sync.sh"
-
-def sync():
-	start = 0
-	end = 0
 
 	#TAKE OUT START FROM PSQL
-	connection = connect()
+	start = None
+	connection = connect_read_db(debug)
 	cursor = connection.cursor()
 	print(cursor)
-	cursor.execute('SELECT * FROM bitcoin_data_app_block_table ORDER BY block_height DESC limit 1')
+	cursor.execute('SELECT * FROM bitcoin_data_app_block_table ORDER BY timestamp DESC limit 1')
 	rows = cursor.fetchall()
 	for row in rows:
 		start = (row[0])
-
 	cursor.close()
 	
+
+
 	#TAKE OUT END FROM BLOCKCHAIN
-	access = connect_blockchain_rpc()
+	access = connect_blockchain_rpc(debug)
 	end = None
 	try:
 		end = access.getblockcount()
@@ -39,31 +36,38 @@ def sync():
 	print(end)
 
 	#commandline and shell scripts
-	if not DEBUG:
+	if not debug:
 		subprocess.check_output(['karan-virtualenv-start'])
 
-	subprocess.check_output(['ls'])
+	# subprocess.check_output(['ls'])
 
-	subprocess.check_call(["sh", sync_loc, str(start), str(end)])
+	# subprocess.check_call(["sh", sync_loc, str(start), str(end)])
 
-	if not DEBUG:
-		subprocess.check_output(['deactivate'])
+	# if not DEBUG:
+		# subprocess.check_output(['deactivate'])
 
 
-def connect_blockchain_rpc():
-	if DEBUG:
-		access = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%('root', 'root'))
+def connect_blockchain_rpc(debug):
+	if debug:
+		print("debug true")
+		access = AuthServiceProxy("http://%s:%s@explorer.blockwala.io:8332"%('karan', 'blockwala@123'))
+		# access = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%('root', 'root'))
 	else:
 		access = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%('karan', 'blockwala@123'))
 	return access
 
-def connect():
+def connect_read_db(debug):
 	try:
-		conn = psycopg2.connect(dbname='bitcoin', user='karan', host='localhost', port='5432', password='blockwala@123')
+		if debug:
+			conn = psycopg2.connect(dbname='bitcoin', user='karan', host='explorer.blockwala.io', port='5432', password='blockwala@123')
+		else:
+			conn = psycopg2.connect(dbname='bitcoin', user='karan', host='localhost', port='5432', password='blockwala@123')
 		return conn
 	except:
-		print "I am unable to connect to the database"
+		print("I am unable to connect to the database")
 		return None
 
-if  __name__ == "__main__":
-	sync()
+#NOTE TX and ouput will be fully synced
+#If you need to sync selective use migrate_data.py
+def run(*args):
+    sync(args[0])
